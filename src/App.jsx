0a2ee660672d749dc3b742fd8e3c95c7b8327623
Rafibinarif1997 <98096402+Rafibinarif1997@users.noreply.github.com>
2026-09-16 01:08:46 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabase";
+import { checkRhoodStoneHolder } from "./lib/rhoodstone";
 import {
   useAppKit,
   useAppKitAccount,
