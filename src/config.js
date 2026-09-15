2450c615a987c6d452bd6339c2c4a059eba652ff
Rafibinarif1997 <98096402+Rafibinarif1997@users.noreply.github.com>
2026-09-15 17:58:43 +0600

    Delete src directory

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://arsriflrliftvpbtzuwt.supabase.co';
-export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyc3JpZmxybGlmdHZwYnR6dXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NTE4NzQsImV4cCI6MjEwNDQyNzg3NH0.-tQ45VyKygjvfea_8lISo_4PDkxjoYa9bUXLsDMA1SU';
-export const OPENSEA_URL = import.meta.env.VITE_OPENSEA_URL || 'https://opensea.io/collection/rhood-stone/activity';
-export const REOWN_PROJECT_ID = import.meta.env.VITE_REOWN_PROJECT_ID || '2b2e1cb4b99f06b269a74445a7e42b55';
--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config';
-export const supabaseUrl = SUPABASE_URL;
-export const supabaseAnonKey = SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
