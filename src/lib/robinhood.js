30ace01ebe67e198cf0da8923dee5ba7b559a164
Rafibinarif1997 <98096402+Rafibinarif1997@users.noreply.github.com>
2026-09-15 23:48:31 +0600

    Create robinhood.js

---
+++ src/lib/robinhood.js
@@ -0,0 +1,16 @@
+export const ROBINHOOD_CHAIN = {
+  chainId: "0x1237",
+  chainIdDecimal: 4663,
+  chainName: "Robinhood Chain",
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: [
+    "https://rpc.mainnet.chain.robinhood.com",
+  ],
+  blockExplorerUrls: [
+    "https://robinhoodchain.blockscout.com",
+  ],
+};
